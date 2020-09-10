751aee348fff4cef066aa917929cf169d07afbc3
spdir <aery_mzc9123@163.com>
2020-09-10 20:23:50 +0800

    tt1

--- data.go
+++ data.go
@@ -1,1 +1,15 @@
 package configer
+
+/* struct tag describe
+
+
+ */
+
+// binding global configure object
+var ConfigerData interface{}
+
+// default parse set value callback funcation binding object
+type defSetCall struct{}
+
+// default get configure value callback fucnation binding object
+type defGetCall struct{}
--- types.go
+++ types.go
@@ -1,1 +1,8 @@
 package configer
+
+// different confiure type interface
+type configInterface interface {
+	loadFromFile(i interface{}, file string) error
+	loadFromByte(i interface{}, data []byte) error
+	getvalueFromSource(key string) (value interface{}, err error)
+}
