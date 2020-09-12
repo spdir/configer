44b35950c93c13ce07514ef67c51d66c9faddfd1
Musker <aery_mzc9123@163.ccom>
2020-09-12 15:56:39 +0800

    reset file struct

--- core.go
+++ core.go
@@ -1,14 +1,126 @@
 package configer
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/tidwall/gjson"
+	"gopkg.in/ini.v1"
+	"gopkg.in/yaml.v2"
 )
 
+// different confiure type interface
+type configInterface interface {
+	loadConfig(i interface{}, file string) error
+	getValue(key string) (value interface{}, err error)
+}
+
+/* configer different configer type module */
+func initialConfigCallObject(configType string) (err error) {
+	fmt.Println("type", configType)
+	switch configType {
+	case "ini":
+		configCallObject = &iniConfig{}
+	case "yaml":
+		configCallObject = &yamlConfig{}
+	case "json":
+		configCallObject = &jsonConfig{}
+	case "toml":
+		configCallObject = &tomlConfig{}
+	default:
+		err = fmt.Errorf("not found %s cofnig type module\n", configType)
+		return
+	}
+	return nil
+}
+
+// ini configure
+type iniConfig struct {
+	configer *ini.File
+}
+
+func (conf *iniConfig) loadConfig(i interface{}, file string) error {
+	iniFile, err := ini.Load(file)
+	if err != nil {
+		return err
+	}
+	conf.configer = iniFile
+	err = iniFile.MapTo(i)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (conf *iniConfig) getValue(key string) (value interface{}, err error) {
+	return
+}
+
+// json configure
+type jsonConfig struct {
+	configer gjson.Result
+}
+
+func (conf *jsonConfig) loadConfig(i interface{}, file string) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(data, i)
+	if err != nil {
+		return err
+	}
+	conf.configer = gjson.ParseBytes(data)
+	return nil
+}
+
+func (conf *jsonConfig) getValue(key string) (value interface{}, err error) {
+	return
+}
+
+// yaml configure
+type yamlConfig struct {
+	configer gjson.Result
+}
+
+func (conf *yamlConfig) loadConfig(i interface{}, file string) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(data, i)
+	if err != nil {
+		return err
+	}
+	conf.configer = gjson.ParseBytes(data)
+	return nil
+}
+
+func (conf *yamlConfig) getValue(key string) (value interface{}, err error) {
+	return
+}
+
+// toml configure
+type tomlConfig struct {
+}
+
+func (conf *tomlConfig) loadConfig(i interface{}, file string) error {
+
+	return nil
+}
+
+func (conf *tomlConfig) getValue(key string) (value interface{}, err error) {
+	return
+}
+
+/* configer parser */
 // parser configure main
 func parserConfig(config interface{}) {
 	scanConfig(config)
@@ -130,3 +242,62 @@ func setConfigValue(fieldType *reflect.StructField, fieldVal *reflect.Value, val
 		fieldVal.SetBool(v)
 	}
 }
+
+/* configer getter */
+// get configure field value
+// example: Get("web::port")
+// example: Get("config")
+func Get(key string) (value interface{}) {
+	return nil
+}
+
+func GetString(key string) (value string) {
+	return
+}
+
+func GetInt(key string) (value int) {
+	return
+}
+
+func GetFloat64(key string) (value float64) {
+	return
+}
+
+func GetBool(key string) (value bool) {
+	return
+}
+
+func GetIntSlice(key string) (value []int) {
+	return
+}
+
+func GetStringMap(key string) (value map[string]interface{}) {
+	return
+}
+
+func GetStringMapString(key string) (value map[string]string) {
+	return
+}
+
+func GetStringSlice(key string) (value []string) {
+	return
+}
+
+func GetTime(key string) (value time.Time) {
+	return
+}
+
+func GetDuration(key string) (value time.Duration) {
+	return
+}
+
+func IsSet(key string) (status bool) {
+	return
+}
+
+func AllSettings() (result map[string]interface{}) {
+	return
+}
+
+/* configer update value */
+// ...
--- types.go
+++
@@ -1,7 +0,0 @@
-package configer
-
-// different confiure type interface
-type configInterface interface {
-	loadConfig(i interface{}, file string) error
-	getValue(key string) (value interface{}, err error)
-}
